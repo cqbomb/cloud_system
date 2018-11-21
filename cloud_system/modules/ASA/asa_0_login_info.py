18abe47bf425002a7f6cd91f1854615181f5a319
cq_bomb <collinsctk@gmail.com>
2018-11-21 09:41:06 +0800

    asa create host

--- cloud_system/modules/ASA/asa_0_login_info.py
+++ cloud_system/modules/ASA/asa_0_login_info.py
@@ -5,12 +5,15 @@
 # 亁颐堂官网www.qytang.com
 # 教主技术进化论拓展你的技术新边疆
 # https://ke.qq.com/course/271956?tuin=24199d8a
+from requests.auth import HTTPBasicAuth
 
 username = 'admin'
 password = 'Cisc0123'
 ip = "192.168.1.104"
 
 my_headers = {"content-type": "application/json"}
+
+auth_header = HTTPBasicAuth(username, password)
 
 
 
--- cloud_system/modules/ASA/asa_1_create_obj.py
+++ cloud_system/modules/ASA/asa_1_create_obj.py
@@ -5,73 +5,48 @@
 # 亁颐堂官网www.qytang.com
 # 教主技术进化论拓展你的技术新边疆
 # https://ke.qq.com/course/271956?tuin=24199d8a
+from asa_0_login_info import ip, my_headers, auth_header
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def create_in_obj(VLANID):
-    username = "admin"
-    password = "Cisc0123"
-    ip = "192.168.1.104"
-    disable_warnings()
-    http = PoolManager()
 
-    ipaddress = "172.16."+str(VLANID)+".100"
-    object_name = "VLAN_" + str(VLANID) + "_HOST"
-    #print('创建ASA内部Object' + object_name)
-    headers = {}
-
-    headers['Content-Type'] = 'application/json'
-
-    user_pass_str = username + ':' + password
-    user_pass_str_encode = user_pass_str.encode()
-    userAndPass = b64encode(user_pass_str_encode).decode("ascii")
-
-    headers["Authorization"] = 'Basic %s' % userAndPass
+def create_in_obj(vid, ip):
+    vlanid = str(vid)
+    ipaddress = "172.16." + vlanid + ".100"
+    object_name = "VLAN_" + vlanid + "_HOST"
 
     json_data = {
-                 "host" : {
-                          "kind" : "IPv4Address",
-                          "value" : ipaddress
+                 "host": {
+                          "kind": "IPv4Address",
+                          "value": ipaddress
                          },
-                 "kind" : "object#NetworkObj",
-                 "name" : object_name
+                 "kind": "object#NetworkObj",
+                 "name": object_name
                  }
     url = 'https://' + ip + '/api/objects/networkobjects'  # 请求的URL
-    r = http.request('POST', url, headers=headers, body=json.dumps(json_data))  # 使用POST发起请求,并且使用认证头部
-    #print(r.data.decode())
+    requests.post(url, headers=my_headers, auth=auth_header, json=json_data, verify=False)  # 使用POST发起请求,并且使用认证头部
 
-def create_out_obj(VLANID):
-    username = "admin"
-    password = "Cisc0123"
-    ip = "192.168.1.104"
 
-    disable_warnings()
-    http = PoolManager()
+def create_out_obj(vid, ip):
+    vlanid = str(vid)
 
-    outside_ip = "202.100.1."+ str(VLANID)
-    object_name = "outside_" + str(VLANID)
-    #print('创建ASA外部Object'+object_name)
-    headers = {}
-
-    headers['Content-Type'] = 'application/json'
-
-    user_pass_str = username + ':' + password
-    user_pass_str_encode = user_pass_str.encode()
-    userAndPass = b64encode(user_pass_str_encode).decode("ascii")
-
-    headers["Authorization"] = 'Basic %s' % userAndPass
+    outside_ip = "202.100.1." + vlanid
+    object_name = "outside_" + vlanid
 
     json_data = {
-                 "host" : {
-                          "kind" : "IPv4Address",
-                          "value" : outside_ip
+                 "host": {
+                          "kind": "IPv4Address",
+                          "value": outside_ip
                          },
-                 "kind" : "object#NetworkObj",
-                 "name" : object_name
+                 "kind": "object#NetworkObj",
+                 "name": object_name
                  }
     url = 'https://' + ip + '/api/objects/networkobjects'  # 请求的URL
-    r = http.request('POST', url, headers=headers, body=json.dumps(json_data))  # 使用POST发起请求,并且使用认证头部
-    #print(r.data.decode())
+    requests.post(url, headers=my_headers, auth=auth_header, json=json_data, verify=False)  # 使用POST发起请求,并且使用认证头部
+
 
 if __name__ == "__main__":
-    create_in_obj(6)
-    create_out_obj(6)
+    create_in_obj(6, ip)
+    create_out_obj(6, ip)
 
