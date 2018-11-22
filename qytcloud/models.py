647ac1b7c6a3184c93ab5123da3a2493bf9371c5
cq_bomb <collinsctk@gmail.com>
2018-11-22 08:16:53 +0800

    数据库条目修改完毕！

--- qytcloud/models.py
+++ qytcloud/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 class Vmdb(models.Model):
     vm_name = models.CharField(max_length=100, blank=False)
-    vm_summary = models.CharField(max_length=10000, blank=False)
+    vm_summary = models.CharField(max_length=10000, blank=True)
     vm_owner = models.CharField(max_length=100, blank=False)
-    vm_webconsole_url = models.CharField(max_length=10000, blank=False)
+    vm_ip = models.CharField(max_length=100, blank=True)
+    vm_global_ip = models.CharField(max_length=100, blank=True)
+    vm_status = models.CharField(max_length=100, blank=True)
+    vm_webconsole_url = models.CharField(max_length=10000, blank=True)
 
 
