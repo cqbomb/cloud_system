a235be76c4fef3e3e4dc4e9f7255dc85a5322131
cq_bomb <collinsctk@gmail.com>
2018-11-23 11:37:54 +0800

    修改数据结构！

--- qytcloud/models.py
+++ qytcloud/models.py
@@ -5,9 +5,14 @@ class Vmdb(models.Model):
     vm_name = models.CharField(max_length=100, blank=False)
     vm_summary = models.CharField(max_length=10000, blank=True)
     vm_owner = models.CharField(max_length=100, blank=False)
-    vm_ip = models.CharField(max_length=100, blank=True)
-    vm_global_ip = models.CharField(max_length=100, blank=True)
+    vm_cpu_cores = models.IntegerField(default=1, blank=False)
+    vm_mem_G = models.IntegerField(default=1, blank=False)
+    vm_nics = models.IntegerField(default=1, blank=False)
+    vm_nics_speed_M = models.IntegerField(default=1, blank=True)
+    vm_disk_space_G = models.IntegerField(default=1, blank=True)
+    vm_ip = models.GenericIPAddressField()
+    vm_global_ip = models.GenericIPAddressField()
     vm_status = models.CharField(max_length=100, blank=True)
-    vm_webconsole_url = models.CharField(max_length=10000, blank=True)
+    vm_webconsole_url = models.URLField(blank=True)
 
 
