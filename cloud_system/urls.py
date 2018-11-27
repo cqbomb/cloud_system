1a199829883c32416bd28a1fb25654393a5f46ab
cqbomb <605658506@qq.com>
2018-11-27 20:46:50 +0800

    注释URL.py

--- cloud_system/urls.py
+++ cloud_system/urls.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """cloud_system URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,15 +19,15 @@ from django.urls import path
 from views import index, qyt_forms, qyt_webconsole, qyt_login, qyt_vm_power_control, qyt_delete_vm, qyt_recovery
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('subscribevm/', qyt_forms.subscribevm),
-    path('accounts/login/', qyt_login.qyt_login),
-    path('accounts/logout/', qyt_login.qyt_logout),
-    path('webconsole/<str:name>/', qyt_webconsole.webconsole),
-    path('myvms/', qyt_forms.myvms),
-    path('poweron/<str:vmname>', qyt_vm_power_control.poweron),
-    path('poweroff/<str:vmname>', qyt_vm_power_control.poweroff),
-    path('delete/<str:vmname>', qyt_delete_vm.delete_vm),
-    path('recovery/', qyt_recovery.recovery_cloud),
-    path('', index.index)
+    path('admin/', admin.site.urls),  # 登录管理
+    path('subscribevm/', qyt_forms.subscribevm),  # 定于虚拟机
+    path('accounts/login/', qyt_login.qyt_login),  # 登录
+    path('accounts/logout/', qyt_login.qyt_logout),  # 登出
+    path('webconsole/<str:name>/', qyt_webconsole.webconsole),  # 访问webconsole
+    path('myvms/', qyt_forms.myvms),  # 显示我的虚拟机
+    path('poweron/<str:vmname>', qyt_vm_power_control.poweron),  # 打开特定虚拟机电源
+    path('poweroff/<str:vmname>', qyt_vm_power_control.poweroff),  # 关闭特定虚拟机电源
+    path('delete/<str:vmname>', qyt_delete_vm.delete_vm),  # 删除特定虚拟机
+    path('recovery/', qyt_recovery.recovery_cloud),  # 重置实验环境
+    path('', index.index)  # 显示主页
 ]
